73fd47eca73934141f13b3c59a5e65f93692ad15
mahendran <mahendran@Mahii.local>
2018-01-10 14:12:45 +0530

    Render based on capitalize params

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -1,5 +1,7 @@
 class DocumentsController < ApplicationController
 	require "base64"
+	require 'net/http'
+
   before_action :authenticate_user
 
   def authenticate_user
@@ -21,19 +23,36 @@ class DocumentsController < ApplicationController
   def create
   	document = Document.new(document_params)
   	if(document.save)
-  		redirect_to document_path(document.id)
+  		redirect_to document_path(document.id, capitalize:true)
   	else
   		redirect_to :back, flash[:errors] => "Failed to Save Document"
   	end
   end
 
   def show
+  	if(params[:capitalize])
+  		show_capitalized
+  	else
+  		@document = Document.find(params[:id])
+  	end
+  end
+
+  def show_capitalized
   	@document = Document.find(params[:id])  
-  	encoded = Base64.encode64(@document.to_xml)
+  	encoded = Base64.encode64(@document.to_xml(skip_instruct: true))
   	decoded = Base64.decode64(encoded)
   	puts "encoded :\n #{encoded}"
   	puts "decoded :\n #{decoded}"
-  	render :xml => @document	
+  	url = URI.parse('http://localhost:3002/capitalize.json')
+  	data = { "data": encoded }
+	res = Net::HTTP.post_form url, data
+	# res = Net::HTTP.start(url.host, url.port) {|http|
+	#   http.request(req)
+	# }
+	puts "response: \n#{res.body}"
+	response_xml = Base64.decode64(res.body)
+	puts "decoded response: \n#{response_xml}"
+  	render :xml => response_xml
   end
 
   private def document_params
