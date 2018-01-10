2ee8646ebe67fb41d487881ece32da29ca59ec78
mahendran <mahendran@Mahii.local>
2018-01-10 12:16:30 +0530

    Initializing repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MyDocuments::Application.config.secret_key_base = '6638000d8621db5fd04efa61eea7ad3fdea0912587c3d490b73c67ac1f9e6aad6874c946a685a8357138f30fd1cb7016fc0f1fe7b661f4480a1a44ce5a065d04'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MyDocuments::Application.config.session_store :cookie_store, key: '_my_documents_session'
